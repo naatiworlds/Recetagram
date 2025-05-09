bc4b098fb81c9da5e5cdbadfee708b97a8ecbb82
naatiworlds <natiworlds@gmail.com>
2025-05-09 18:56:25 +0200

    Feature: Añadiendo PWA para comodidad del usuario

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "tpv-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/src/main.jsx",
+  "/css/index.css",
+  "/images/logo.png",
+  // Agrega aquí otros recursos que quieras cachear
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Archivos cacheados");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Cache antiguo eliminado:", cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
