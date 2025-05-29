4d2a9323cb5e1ea5a27b4eb799540835abc9cee6
naatiworlds <natiworlds@gmail.com>
2025-05-29 10:45:32 +0200

    Fix: Cambiando de backend por limite de prueba gratuita

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ import { useUserStore } from "@/stores/user";
 
 // Configura Axios con las opciones básicas
 const api = axios.create({
-  baseURL: "https://recetagrm-api.onrender.com/api/v1",
+  baseURL: "https://recetagram-api.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
