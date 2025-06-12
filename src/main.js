1a929ea5634843a34fd3d6ceb02dc1d13373118a
naatiworlds <natiworlds@gmail.com>
2025-06-12 22:45:04 +0200

    d

--- src/main.js
+++ src/main.js
@@ -51,9 +51,6 @@ app.config.errorHandler = (error, vm, info) => {
   notificationStore.show('Ha ocurrido un error inesperado', 'error');
 };
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 5-procesamiento-de-datos-por-lotes
 export default app;
