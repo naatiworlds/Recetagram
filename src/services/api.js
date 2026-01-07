2650b98ebf92fbb0a2e35084ad532f2e5fa40902
naatiworlds <natiworlds@gmail.com>
2026-01-07 18:47:35 +0100

    Backup 7-1-2026

--- src/services/api.js
+++ src/services/api.js
@@ -211,7 +211,7 @@ export const apiService = {
     return api
       .post("/fcm-token", { fcm_token: formattedToken })
       .then((response) => {
-  // Token FCM sent to backend
+        // Token FCM sent to backend
         return response.data;
       })
       .catch((err) => {
@@ -228,7 +228,7 @@ export const apiService = {
   sendBatchRequests: async (batchData) => {
     try {
       const response = await api.post("/batch", batchData);
-  // batch requests sent successfully
+      // batch requests sent successfully
       return response.data;
     } catch (error) {
       console.error("[API] Error al enviar peticiones en lote:", error);
