eecb485b944eece83e0415cbf976e649dc55e23e
naatiworlds <natiworlds@gmail.com>
2026-01-30 23:59:36 +0100

    Fix: Reemplazar íconos de Vue con logo de Recetagram

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Nombre del caché - Cambiar la versión cuando hay actualizaciones
-const CACHE_NAME = "recetagram-cache-v4";
+const CACHE_NAME = "recetagram-cache-v5";
 
 // Archivos a cachear
 const urlsToCache = [
